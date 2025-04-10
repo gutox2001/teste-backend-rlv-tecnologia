120cff5060f3f8a2c3fd62b4110ee5802c98e3b8
gutox2001 <aroldoaugusto10@gmail.com>
2025-04-10 01:36:00 -0300

    :sparkles: feat: Update main.ts ->  initial swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+
+import { EnvService } from './common/env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Teste Técnico RLV Tecnologia - Aroldo Augusto')
+    .setDescription('API Rest criada com Nest, Postgres , TypeORM e .')
+    .setVersion('0.1')
+    .addServer('http://localhost:3000/', 'Local environment')
+    // .addServer('https://', 'Production')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  
+  const configService = app.get(EnvService);
+  const port = configService.get('PORT_NEST_LOCALHOST');
+
+  await app.listen(port);
 }
 bootstrap();
