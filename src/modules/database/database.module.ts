c8c2eb45b2998383b112dcf430f1c71446f7ffc7
gutox2001 <aroldoaugusto10@gmail.com>
2025-04-12 01:02:12 -0300

    :sparkles: boom: Update DatabaseModule - constants and db providers

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
+import { cityProviders, databaseProviders, stateProviders } from './database.providers';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [
-        ...databaseProviders
-    ],
-    exports: [
-        ...databaseProviders
-    ],
+	imports: [],
+	controllers: [],
+	providers: [
+		...databaseProviders,
+		...cityProviders, 
+		...stateProviders,
+	],
+	exports: [...databaseProviders, ...cityProviders, ...stateProviders],
 })
 export class DatabaseModule {}
--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -1,27 +1,42 @@
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
+import { CITY_REPOSITORY, DATA_SOURCE, STATE_REPOSITORY } from './constants';
+import { City } from '../cities/entities/city.entity';
+import { State } from '../states/entities/state.entity';
 
 /*
     Provider utilizado para conectar ao banco de dados.
 */
 export const databaseProviders = [
-    {
-        provide: 'DATA_SOURCE',
-        useFactory: async () => {
-            return new DataSource({
-                type: 'postgres',
-                host: 'localhost',
-                port: Number(process.env.DATABASE_PORT),
-                username: process.env.DATABASE_USERNAME,
-                password: process.env.DATABASE_PASSWORD,
-                database: process.env.DATABASE_NAME,
-                synchronize: true,
-                logging: true,
-                entities: [
-                    // aqui você pode adicionar suas entidades
-                    // User,
-                    // Post,
-                ],
-            }).initialize();
-        },
-    },
+	{
+		provide: DATA_SOURCE,
+		useFactory: async () => {
+			return new DataSource({
+				type: 'postgres',
+				host: 'localhost',
+				port: Number(process.env.DATABASE_PORT),
+				username: process.env.DATABASE_USERNAME,
+				password: process.env.DATABASE_PASSWORD,
+				database: process.env.DATABASE_NAME,
+				synchronize: true,
+				logging: true,
+				entities: [City, State],
+			}).initialize();
+		},
+	},
+];
+
+export const cityProviders = [
+	{
+		provide: CITY_REPOSITORY,
+		useFactory: (dataSource: DataSource) => dataSource.getRepository(City),
+		inject: [DATA_SOURCE],
+	},
+];
+
+export const stateProviders = [
+	{
+		provide: STATE_REPOSITORY,
+		useFactory: (dataSource: DataSource) => dataSource.getRepository(State),
+		inject: [DATA_SOURCE],
+	},
 ];
